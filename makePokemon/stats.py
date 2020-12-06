89bfefdb46aa133052391ace9b43195f26ac9607
ogadra <ogaryu0529@gmail.com>
2020-12-06 22:30:00 +0900

    EVcalc algorithm update

--- makePokemon/stats.py
+++ makePokemon/stats.py
@@ -16,14 +16,34 @@ def choicePoint(nature, count):
     if nature['name'] == 'Serious':
         points = np.random.choice(stasList, size=count, replace=False)
     elif count == 3:
-        prob = np.array([0.1]*6)
+        prob = np.array([0.125]*6)
         prob[stasList.index(nature['plus'])] = 0.5
+        prob[stasList.index(nature['minus'])] = 0
         points = np.random.choice(stasList, size=3, replace=False, p=prob)
     elif count == 5:
         points = copy(stasList)
         points.remove(nature['minus'])
 
     return points
+
+def EVcalc(pokemon, nature, count):
+    w = [0] * 6
+    points = choicePoint(nature, count)
+    for i in points:
+        w[stasList.index(i)] = 1
+        pokemon['EVs'][i] = 4
+    if nature['plus'] in points:
+        w[stasList.index(nature['plus'])] += 1
+    
+    roopCount = 62 if count == 3 else 61
+    for i in range(roopCount):
+            incrementPoint = random.choices(stasList, weights=w)[0]
+            pokemon['EVs'][incrementPoint] += 8
+            if pokemon['EVs'][incrementPoint] == 252:
+                w[stasList.index(incrementPoint)] = 0
+            else:
+                w[stasList.index(incrementPoint)] += 1
+    return pokemon
 
 def generate():
     pokemon = {'EVs':{'HP':0, 'Atk':0, 'Def':0, 'SpA':0, 'SpD':0, 'Spe':0}, 'IVs':{'HP':31, 'Atk':31, 'Def':31, 'SpA':31, 'SpD':31, 'Spe':31}}
@@ -37,8 +57,8 @@ def generate():
     pokemon.update(nature)
 
     if nature['minus'] == 'Atk' or nature['minus'] == 'Spe':
-        pokemon['IVs'][nature['minus']] = 0 if random.randrange(2) else 31
-
+        pokemon['IVs'][nature['minus']] = 0 if random.randrange(3) else 31
+    
     if pChoice(0.9):
         # 3points
         if pChoice(0.8):
@@ -67,40 +87,9 @@ def generate():
                 pokemon['EVs'][points[-1]] = 4
 
         else:
-            w = [0]*6
-            points = choicePoint(nature, 3)
-            
-            for i in points:
-                w[stasList.index(i)] = 1
-                pokemon['EVs'][i] = 4
-
-            if nature['plus'] in points:
-                w[stasList.index(nature['plus'])] += 1
-            
-            for i in range(62):
-                incrementPoint = random.choices(stasList, weights=w)[0]
-                pokemon['EVs'][incrementPoint] += 8
-                if pokemon['EVs'][incrementPoint] == 252:
-                    w[stasList.index(incrementPoint)] = 0
-                else:
-                    w[stasList.index(incrementPoint)] += 1
+            pokmeon = EVcalc(pokemon, nature, 3)
     else:
-        points = choicePoint(nature, 5)
-        w = [0]*6
-
-        for i in points:
-            w[stasList.index(i)] = 1
-            pokemon['EVs'][i] = 4
-        if nature['plus'] in points:
-            w[stasList.index(nature['plus'])] += 1
-
-        for i in range(61):
-            incrementPoint = random.choices(stasList, weights=w)[0]
-            pokemon['EVs'][incrementPoint] += 8
-            if pokemon['EVs'][incrementPoint] == 252:
-                w[stasList.index(incrementPoint)] = 0
-            else:
-                w[stasList.index(incrementPoint)] += 1
+        pokemon = EVcalc(pokemon, nature, 5)
 
         # 5points
         pass
@@ -110,3 +99,8 @@ if __name__ == '__main__':
     pokemon = generate()
 
     # print(sum(generate()['EVs'].values()))
+
+    # for i in range(100000):
+    #     tmp = generate()
+    #     if sum(tmp['EVs'].values()) != 508:
+    #         print(tmp)

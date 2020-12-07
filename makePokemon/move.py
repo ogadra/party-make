8ea716320cd25d40503fc33fcae64f3d8147f2e0
ogadra <ogaryu0529@gmail.com>
2020-12-08 00:29:32 +0900

    move choice bug fixed

--- makePokemon/move.py
+++ makePokemon/move.py
@@ -7,11 +7,13 @@ moves = json.load(open('./data/learnsets.json'))
 
 def selectMove(pokemon):
     ableMoves = [i for i, val in moves[pokemon]['learnset'].items() if [j for j in val if re.match('8\w*', j)]]
-    selectedMoves = list(random.choice(ableMoves, 4))
+    selectedMoves = list(random.choice(ableMoves, 4, replace=False))
     return selectedMoves
 
 if __name__ == '__main__':
     # moves = selectMove('nidoranm')
-    for i in range(500):
-        print(selectMove('nidoranm'))
-        time.sleep(0.1)
+    for i in range(50000):
+        tmp = selectMove('wooper')
+        if len(set(tmp)) != 4:
+            print(tmp)
+        # time.sleep(0.5)

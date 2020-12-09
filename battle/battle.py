f15668bf607bd54ad7db15db95256b79268792a4
ogadra <ogaryu0529@gmail.com>
2020-12-10 01:15:24 +0900

    battle sim

--- battle/battle.py
+++ battle/battle.py
@@ -1,9 +1,39 @@
 import subprocess
+from time import time
+import multiprocessing
+import sys, os
+sys.path += [os.path.dirname(os.path.dirname(__file__))]
 
-pt1 = "|vanillite|Leppa Berry|Snow Cloak|irondefense,endure,iciclecrash,lightscreen|Impish|252,116,124,0,12,4||31,31,31,31,31,31||50|"
-pt2 = "|riolu|Muscle Band|Prankster|attract,drainpunch,dig,detect|Timid|4,0,0,252,0,252||31,0,31,31,31,31||50|"
-result = subprocess.run(['node', './bot/.sim-dist/examples/battle-stream-example', pt1, pt2], stdout=subprocess.PIPE, text=True)
-data = result.stdout
-print(data)
-data = data.split('\n')[-2]
-print(data)
+# from makePokemon import makePokemon
+
+pt1 = '|darumakagalar|Liechi Berry|Hustle|hammerarm,icefang,fireblast,focuspunch|Hasty|84,124,0,76,132,92||31,31,31,31,31,31||50|'
+pt2 = '|darumakagalar|Liechi Berry|Hustle|hammerarm,icefang,fireblast,focuspunch|Hasty|84,124,0,76,132,92||31,31,31,31,31,31||50|'
+
+def battle():
+    result = subprocess.run(['node', './bot/.sim-dist/examples/battle-stream-example', pt1, pt2], stdout=subprocess.PIPE, text=True)
+
+
+if __name__ == "__main__":
+    # pt1s = list()
+    # for i in range(100):
+    #     pt1s.append(makePokemon.makeParties())
+    # pt2s = list()
+    # for i in range(100):
+    #     pt2s.append(makePokemon.makeParties())
+    # data = result.stdout
+    # print(data)
+
+    jobs = []
+    cnt = 100
+
+    start = time()
+    
+    for i in range(cnt):
+        p = multiprocessing.Process(target=battle)
+        jobs.append(p)
+        p.start()
+        
+    p.join()
+    print((time() - start)/cnt)
+    # data = data.split('\n')[-2]
+# print(data)
--- makePokemon/makePokemon.py
+++ makePokemon/makePokemon.py
@@ -1,20 +1,44 @@
+# from makePokemon import item
+# from makePokemon import ability
+# from makePokemon import move
+# from makePokemon import stats
+
 import item
 import ability
 import move
 import stats
+
 from numpy import random
 import json
 import time
 
-def returnValues(vals):
-    tmp = [[val, key] for key, val in vals.items()]
-    vals = str()
-    for i in range(6):
-        vals += str(tmp[i][0]) + ' ' + str(tmp[i][1])
-        if i != 5:
-            vals += ' / '
+pokemons = open('./data/delibird-single-eng.txt').read().split('\n')
+with open('./data/pokedex.json') as f:
+    pokedex = json.load(f)
 
-    return vals
+def makeParties(cnt=1):
+    # format = "NICKNAME|SPECIES|ITEM|ABILITY|MOVES|NATURE|EVS|GENDER|IVS|SHINY|LEVEL|HAPPINESS,POKEBALL,HIDDENPOWERTYPE"
+    party = str()
+
+    for i in range(cnt):
+        pokemon = [''] * 9
+        pokemon[1] = random.choice(pokemons)
+
+        pokemon[2] = item.selectItem(pokemon[1])
+        pokemon[3] = ability.selectability(pokemon[1])
+        # pokemon[4] = stats.generate()
+        pokemon[4] = move.selectMove(pokemon[1])
+        pokemon[5] = stats.showdownpt()
+        pokemon[7] = 50
+        pokemon = list(map(str, pokemon))
+        party += '|'.join(pokemon)
+        if not i == cnt - 1:
+            party += ']'
+        else:
+            pass
+    return party
+
+
 
 if __name__ == "__main__":
     pokemons = open('./data/delibird-single-eng.txt').read().split('\n')

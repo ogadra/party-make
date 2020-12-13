694acaa619bce0fac9efe492f3e9d7977c7c912b
ogadra <ogaryu0529@gmail.com>
2020-12-13 16:43:55 +0900

    before branch commit

--- battle/battle.py
+++ battle/battle.py
@@ -13,40 +13,14 @@ def battle():
     battleData = proc.stdout.split('\n')
     result = [i for i in battleData if re.match('\|win\|Bot \d',i)]
     # result = proc.stdout.split('\n')[-2]
-    result = str(result[0])
+    result = str(result)[0]
     return result
 
 
 if __name__ == "__main__":
     # pt1s = list()
     print(makePokemon.makeParties())
-    for i in range(1000):
-        tmp = battle()
-        if tmp == '|win|Bot 1':
-            pass
-        elif tmp == '|win|Bot 2':
-            pass
-        else:
-            print(tmp)
-    # for i in range(100):
-    #     pt1s.append(makePokemon.makeParties())
-    # pt2s = list()
-    # for i in range(100):
-    #     pt2s.append(makePokemon.makeParties())
-    # data = result.stdout
-    # print(data)
+    print(battle())
 
-    # jobs = []
-    # cnt = 1000
 
-    # start = time()
-    
-    # for i in range(cnt):
-    #     p = multiprocessing.Process(target=battle)
-    #     jobs.append(p)
-    #     p.start()
-        
-    # p.join()
-    # print((time() - start)/cnt)
-    # data = data.split('\n')[-2]
 # print(data)
--- ga-eval.py
+++ ga-eval.py
@@ -1,7 +1,7 @@
 from battle import TrueSkillModule
 from ga import gaModule
 
-pokemon = 'garchomp'
+pokemon = 'abra'
 for i in range(100):
     TrueSkillModule.evalBattle(pokemon)
     gaModule.ga(pokemon)

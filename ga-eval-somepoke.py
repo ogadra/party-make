428779840d0930b52f5c6eebb57afe7ab4832ed6
ogadra <ogaryu0529@gmail.com>
2020-12-18 18:08:22 +0900

    delete test code

--- ga-eval-somepoke.py
+++ ga-eval-somepoke.py
@@ -22,8 +22,7 @@ if __name__ == '__main__':
             nextGeneration = list()
             for i in range(5):
                 nextGeneration.extend(gaModule.ga(pokemons[i*20:(i+1)*20], 10))
-            
-            print(*nextGeneration, sep='\n')
+
             with open(path + 'generate' + str(j+1).zfill(3) + '.txt', 'w') as f:
                 f.write('\n'.join(nextGeneration))
 

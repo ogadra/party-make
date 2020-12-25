e61fd8c75fbaece8c0d35cf31e6c25a7d542414f
ogadra <ogaryu0529@gmail.com>
2020-12-26 01:26:25 +0900

    FtFeval implemented

--- FtFeval.py
+++ FtFeval.py
@@ -28,29 +28,32 @@ def wrapper(args):
 
 if __name__ == '__main__':
     path = './train_data03/'
-    dataSet = open(path + 'default.txt').read().split('\n')
+    opponent = open(path + 'extract.txt').read().split('\n')
     speciesCnt = 80
-    opponent = [dataSet[i*24:i*24+5] for i in range(speciesCnt)]
-    prop = [dataSet[i*24] for i in range(speciesCnt)]
+    prop = [opponent[i*5] for i in range(speciesCnt)]
     battleData = list()
 
     #battle count = 80 * 80 * 5 => 32000 (about 12800s, 3.5h)
     for i in prop:
+        print('\r%d' % len(battleData), end='')
         tmp = list()
-        que = [[i,j] for j in opponent]
+        que = [[i,opponent[j*5:j*5+5]] for j in range(speciesCnt)]
+
         core = cpu_count()
         p = Pool(core)
         tmp = p.map(wrapper, que)
-        battleData.append([i]+[tmp])
-
-        print(battleData)
-        exit()
+        poke = [i.split('|')[1]]
+        poke.extend(tmp)
+        poke = [str(j) for j in poke]
+        battleData.append(poke)
+        
         # for test
     
     pokemons = [i.split('|')[1] for i in prop]
     pokemons = ['FtF'] + pokemons
     with open(path + 'FtFvalue.txt', 'w') as f:
         f.write(','.join(pokemons))
+        f.write('\n')
         for i in battleData:
             f.write(','.join(i))
             f.write('\n')
